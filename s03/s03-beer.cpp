7ca9e4aaf3a1ec5f448cf9d8dc9d91bfe516c623
Philipp Murawski <91725270+PhilippMurawski@users.noreply.github.com>
2021-11-30 21:45:45 +0100

    upd

--- s03/s03-beer.cpp
+++ s03/s03-beer.cpp
@@ -2,15 +2,25 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[]) // argc oznacza liczbę argumentów, a argv oznacza wartości argumentów. Są to zmienne przekazywane do funkcji main, gdy program zaczyna ona wykonywać polecenia.
 {
-	int i = 99;
-	
-	while( i > 0 )
+	if(argc <=1) // Jeśli nie wpiszemy argumentu program wyrzuci cout << " Nie podano argumentu ! \n "
 	{
-			cout << i << " Bottles of beer on the wall, "<< i <<" bottles of beer. Take on down pass it around... \n";
-			i--;
-			
+		cout << " Nie podano argumentu ! \n ";
+		
+		return 1;
 	}
-			cout << " No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...\n";
-}
+	
+	auto const a = stoi(argv[1]); // przypisuje do stałej const a (która jest typem automatycznym bo ma auto) i przypisuje do niej wartość z tabilicy arv[1] która jest zmieniana z stoi(string to int) 
+
+	for(auto i=a; i>=0; i--) // i=a ustawia zmienna przed rozpoczeciem i>=0 ustala warunek dla pętli zeby zadziałała jeśli warunek jest prawdziwy petla bedzie sie za pętlać jesli bedzie faszł to sie zakończy i-- to za kazdym razem jak  sie wykona bedzie zmniejszac wartosc
+	{
+			while( i > 0 )
+		{
+			cout << i << " Bottles of beer on the wall, "<< i <<" bottles of beer. Take on down pass it around... \n";
+			i--;	
+		}
+			cout << " No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, " << a << " bottles of beer on the wall...\n ";
+	}
+}	
+
--- s03/s03-fizzbuzz.cpp
+++ s03/s03-fizzbuzz.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+auto main(int argc, char *argv[]) -> int // argc oznacza liczbę argumentów, a argv oznacza wartości argumentów. Są to zmienne przekazywane do funkcji main, gdy program zaczyna ona wykonywać polecenia.
 {
-	int x;
-	cout << "Podaj liczbe:\n";
-	cin >> x;
-	cout << "Podana liczba:" << x << "\n";
-	
-	if( x % 3 == 0 && x % 5 == 0)
-	{
-		cout << "FizzBuzz\n";
-	}	
-	else if(x % 5 == 0)
-	{
-		cout << "Buzz\n";
-	}
-	else if(x % 3 == 0)
-	{
-		cout << "Fizz\n";
-	}
-}
+    if (argc <= 1) // Jeśli nie wpiszemy argumentu program wyrzuci cout << " Nie podano argumentu ! \n "
+    {
+        cout << "Nie podano argumentu! /n";
+        return 1;
+    }
+    auto const a = stoi(argv[1]); // przypisuje do stałej const a (która jest typem automatycznym bo ma auto) i przypisuje do niej wartość z tabilicy arv[1] która jest zmieniana z stoi(string to int) 
 
+    for (int i = 1; i <= a; i++) // i=a ustawia zmienna przed rozpoczeciem i<=a ustala warunek dla pętli zeby zadziałała jeśli warunek jest prawdziwy petla bedzie sie za pętlać jesli bedzie faszł to sie zakończy i++ to za kazdym razem jak  sie wykona bedzie zmniejszac wartosc
+    {
+        string output = "";
+
+        if (i % 3 == 0) output += "Fizz"; // jeśli warunek zostanie spełniony to string output wydruguje fizz
+        if (i % 5 == 0) output += "Buzz"; // jeśli warunek zostanie spełniony to string output wydruguje buzz
+        else if (output == "") output = to_string(i); // instrukcja określająca nowy warunek, jeśli pierwszy warunek jest fałszywy 
+        
+        cout << output << "\n";
+    }
+
+    return 0;
+}
